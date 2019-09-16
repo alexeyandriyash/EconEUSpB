12693755a8737e5ba201c379337dfc9616a91330
Alexey Andriyash <alexey@andriya.sh>
2019-09-16 18:44:52 +0300

    new scale

--- dynamic/game-1/game-1.py
+++ dynamic/game-1/game-1.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 header = 'Game#1 by Andriyash, Babkina, Tokareva'
 plotTitle = r'$\frac{dy}{dx} = y^2 - x^2$'
-scale = 1000.0
-D = 30
+scale = 1.0
+D = 10
 
 #get x, y vector of points
 oX = np.linspace(-scale, scale, D)
@@ -12,14 +12,14 @@ oY = np.linspace(-scale, scale, D)
 
 #get grid of points
 vX, vY = np.meshgrid(oX, oY)
-deg = np.arctan(vX*vX - vY*vY)
+deg = np.arctan(vY*vY-vX*vX)
 dx = np.cos(deg)
 dy = np.sin(deg)
 
 plt.figure(header)
 plt.title(plotTitle)
-QP = plt.quiver(vX, vY, dx, dy, deg, units='x')
-plt.scatter(vX, vY, color='r', s=5)
-plt.scatter(vX, vY, color='0.8', s=1)
+QP = plt.quiver(vX, vY, dx, dy)
+#plt.scatter(vX, vY, color='r', s=5)
+#plt.scatter(vX, vY, color='0.8', s=1)
 
 plt.show()
